88f52546822535f9ad2cfa7bbe92c9c262422bec
Igor Telles <igortelles09@gmail.com>
2025-05-22 10:51:46 -0300

    mochila funcionando

--- knapsack.py
+++ knapsack.py
@@ -18,61 +18,47 @@ class Knapsack:
                         their profits, weights, and penalty sets.
         """
         self._max_weight = data.get_knapsack_max_weight()
+        self._weights = data.get_weights()
         self._profits = data.get_initial_profits()
         self._forfeit_sets = data.get_forfeit_sets() 
         self._forfeit_limits = data.get_forfeit_limits()
         self._forfeit_penalties = data.get_forfeit_penalties()
-        self._weights = data.get_weights()
 
-        self._items = set()
-        self._forfeit_usages = np.zeros(len(self._forfeit_limits))
-        self._weight = 0
-        self._profit = 0
+        self._items = np.zeros(len(self._profits))
+        self._penalties = np.zeros( (len(self._profits), len(self._profits)) )
+        self._set_penalties()
 
-    def __repr__(self):
-        """
-        Return a string representation of the Knapsack.
 
-        Returns:
-            str: A string showing the current items, weight, and profit of the knapsack.
+    def _set_penalties(self):
         """
-        return f'Mochila: {self._items}, Peso: {self._weight}, Lucro: {self._profit}'
-    
-    def is_valid(self):
+        Set the penalties matrix. It's a square matrix where the element (i,j) is the penalty of item i if item j is in the same penalty set.
         """
-        Check if the knapsack is valid.
+        for i in range(len(self._forfeit_penalties)):
+            squared = np.outer(self._forfeit_sets[:,i], self._forfeit_sets[:,i])
+            np.fill_diagonal(squared, 0)
+            self._penalties += squared * self._forfeit_penalties[i]
 
-        Returns:
-            bool: True if the knapsack is valid, False otherwise.
-        """
-        return self._weight <= self._max_weight
-
-    def get_max_weight(self):
-        """
-        Get the maximum weight capacity of the knapsack.
-
-        Returns:
-            int: The maximum weight capacity.
-        """
-        return self._max_weight
-    
-    def get_weight(self):
-        """
-        Get the current weight of the knapsack.
-
-        Returns:
-            int: The current total weight of items in the knapsack.
-        """
-        return self._weight
-    
     def get_profit(self):
         """
-        Get the current total profit of the knapsack.
-
-        Returns:
-            int: The current total profit considering all items and their penalties.
+        Get the profit of the knapsack.
         """
-        return self._profit
+        penalties =  self._items @ (self._penalties @ self._items.T)/2
+        profits = self._profits @ self._items.T
+        return profits - penalties
+
+    
+    def get_potential_profits(self):
+        """
+        Get the potential profits of the items outside the knapsack.
+        """
+        current_profits = self._profits * (1 - self._items)
+        return current_profits - (self._penalties @ self._items.T)
+    
+    def get_cost_benefit_ratio(self):
+        """
+        Get the cost benefit ratio of the items outside the knapsack.
+        """
+        return self.get_potential_profits() / self._weights
 
     def add_item(self, item_id):
         """
@@ -81,77 +67,26 @@ class Knapsack:
         Args:
             item_id (int): The ID of the item to add.
         """
-        if item_id not in self._items:
-            self._weight += self._weights[item_id] 
-            self._profit += self._profits[item_id]
-            self._items.add(item_id)
-            self._update_profits(item_id)
-
-    def get_items(self):
+        if self._items[item_id] == 0:
+            self._items[item_id] = 1 
+    
+    def remove_item(self, item_id):
         """
-        Get the set of items currently in the knapsack.
-
-        Returns:
-            set: A set containing the IDs of all items in the knapsack.
-        """
-        return self._items
-
-    def _update_profits(self, item_id):
-        """
-        Update the profits of items based on the current state of penalty sets.
-
-        This method is called when an item is added or removed from the knapsack.
-        It updates the profits of all items that share penalty sets with the modified item.
+        Remove an item from the knapsack.
 
         Args:
-            item_id (int): The ID of the item that was added or removed.
-            remove (bool, optional): If True, the item is being removed. If False, it's being added.
-                                    Defaults to False.
+            item_id (int): The ID of the item to remove.   
         """
-        # Vetor com 1 para cada conjunto que o item_id participa
-        item_forfeit_sets = self._forfeit_sets[item_id]
-        
-        # Atualiza o uso de cada conjunto
-        for i in range(len(item_forfeit_sets)):
-            # Itera pelos conjuntos que o item_id está
-            if item_forfeit_sets[i] == 1:
-                self._forfeit_usages[i] += 1
-        
-        # Calcula o valor de penalidades para cada conjunto.
-        penalties = (self._forfeit_limits - self._forfeit_usages) * self._forfeit_penalties
-        
-        for i in range(len(item_forfeit_sets)):
-            # Itera pelos conjuntos i que o item_id está
-            if item_forfeit_sets[i] == 1:
-                # Itera pelos itens j de cada conjunto i
-                for j in range(len(self._profits)):
-                    if self._forfeit_sets[j, i] == 1 and j not in self._items:
-                        # Atualiza o lucro do item j
-                        self._profits[j] = self._profits[j] + penalties[i]
-                
-        # Se o item já está na mochila, seu lucro já está sendo considerado no total.
-        self._profits[item_id] = 0
-    def get_all_items_profits(self):
+        if self._items[item_id] == 1:
+            self._items[item_id] = 0
+
+    def is_valid(self):
         """
-        Get the current profits of all items, considering penalties.
-        If the item is already in the knapsack, its profit is 0. 
+        Check if the knapsack is valid.
 
         Returns:
-            numpy.ndarray: Array containing the current profit values for all items.
+            bool: True if the knapsack is valid, False otherwise.
         """
-        return self._profits
+        weight = self._weights @ self._items.T
+        return weight <= self._max_weight
     
-    def get_all_items_weights(self):
-        """
-        Get the weights of all items.
-
-        Returns:
-            numpy.ndarray: Array containing the weight values for all items.
-        """
-        return self._weights
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ dotenv.load_dotenv()
 INSTANCES_PATH = os.getenv('INSTANCES')
 from data import Data
 from knapsack import Knapsack
+import numpy as np
 
 
 data_1 = Data(f'{INSTANCES_PATH}/scenario1/correlated_sc1/300/kpfs_1.txt')
 
 m = Knapsack(data_1)
 m.add_item(0)
-m.add_item(299)
+print(np.argsort(m.get_cost_benefit_ratio(), kind='heapsort')[::-1])
 m.add_item(98)
-print(m)
+m.add_item(263)
+m.remove_item(98)
 print(m.is_valid())
-print((m.get_all_items_profits() / m.get_all_items_weights())[0:10])
